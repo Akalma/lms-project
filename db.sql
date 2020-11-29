4acde11563b519c9c9b3c7494750b5a5bb8900b2
Satyajit <websoft1515>
2020-11-29 11:53:44 +0530

    db modified

--- db.sql
+++ db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 25, 2020 at 11:04 AM
+-- Generation Time: Nov 27, 2020 at 06:15 PM
 -- Server version: 10.4.11-MariaDB
 -- PHP Version: 7.4.5
 
@@ -24,62 +24,95 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `data`
+-- Table structure for table `appusers`
 --
 
-CREATE TABLE `data` (
+CREATE TABLE `appusers` (
   `id` int(11) NOT NULL,
-  `first_name` varchar(255) NOT NULL,
-  `last_name` varchar(255) NOT NULL,
-  `mobile` varchar(255) NOT NULL,
-  `area` varchar(255) NOT NULL,
-  `existing_broadband` varchar(255) NOT NULL,
-  `lead_type` varchar(255) NOT NULL,
-  `creared_date` datetime NOT NULL DEFAULT current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data for table `data`
---
-
-INSERT INTO `data` (`id`, `first_name`, `last_name`, `mobile`, `area`, `existing_broadband`, `lead_type`, `creared_date`) VALUES
-(1, 'Satyajit', 'Sadhukhan', '+918442922399', 'Value 1', 'Value 1', '1', '2020-11-25 12:07:48'),
-(2, 'John', 'Doe', '985458569', 'Kolkata', 'Meghbala', 'Hot', '2020-11-25 12:43:39');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `users`
---
-
-CREATE TABLE `users` (
-  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
   `email` varchar(255) NOT NULL,
   `password` varchar(255) NOT NULL,
+  `type` int(11) NOT NULL COMMENT '0=Normal 1=Admin',
   `created` datetime NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data for table `users`
+-- Dumping data for table `appusers`
 --
 
-INSERT INTO `users` (`id`, `email`, `password`, `created`) VALUES
-(1, 'admin@gmail.com', 'e10adc3949ba59abbe56e057f20f883e', '0000-00-00 00:00:00');
+INSERT INTO `appusers` (`id`, `name`, `email`, `password`, `type`, `created`) VALUES
+(1, 'Admin', 'admin@gmail.com', 'e10adc3949ba59abbe56e057f20f883e', 1, '0000-00-00 00:00:00'),
+(3, 'Normal User', 'admin2@gmail.com', 'e10adc3949ba59abbe56e057f20f883e', 0, '0000-00-00 00:00:00');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `lead`
+--
+
+CREATE TABLE `lead` (
+  `id` int(11) NOT NULL,
+  `first_name` varchar(255) DEFAULT NULL,
+  `last_name` varchar(255) DEFAULT NULL,
+  `mobile` varchar(255) DEFAULT NULL,
+  `area` varchar(255) DEFAULT NULL,
+  `existing_broadband` varchar(255) DEFAULT NULL,
+  `lead_type` enum('HOT','WARM','COLD','') DEFAULT NULL,
+  `added_by` int(11) DEFAULT NULL,
+  `creared_date` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `lead`
+--
+
+INSERT INTO `lead` (`id`, `first_name`, `last_name`, `mobile`, `area`, `existing_broadband`, `lead_type`, `added_by`, `creared_date`) VALUES
+(7, 'Satyajit', 'Sadhukhan', '+918442922399', 'Value 1', 'Value 1', 'HOT', 3, '2020-11-27 22:01:43'),
+(8, 'John', 'Doe', '9184429223', 'Value 1', 'Value 1', 'COLD', 3, '2020-11-27 22:02:19'),
+(9, 'John', 'Doe', '9184429223', 'Value 1', 'Value 1', 'COLD', 1, '2020-11-27 22:02:19');
+
+-- --------------------------------------------------------
+
+--
+-- Stand-in structure for view `vw_appusers`
+-- (See below for the actual view)
+--
+CREATE TABLE `vw_appusers` (
+`id` int(11)
+,`first_name` varchar(255)
+,`last_name` varchar(255)
+,`mobile` varchar(255)
+,`area` varchar(255)
+,`existing_broadband` varchar(255)
+,`lead_type` enum('HOT','WARM','COLD','')
+,`added_by` int(11)
+,`creared_date` datetime
+,`name` varchar(255)
+);
+
+-- --------------------------------------------------------
+
+--
+-- Structure for view `vw_appusers`
+--
+DROP TABLE IF EXISTS `vw_appusers`;
+
+CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `vw_appusers`  AS  select `lead`.`id` AS `id`,`lead`.`first_name` AS `first_name`,`lead`.`last_name` AS `last_name`,`lead`.`mobile` AS `mobile`,`lead`.`area` AS `area`,`lead`.`existing_broadband` AS `existing_broadband`,`lead`.`lead_type` AS `lead_type`,`lead`.`added_by` AS `added_by`,`lead`.`creared_date` AS `creared_date`,`appusers`.`name` AS `name` from (`lead` join `appusers` on(`appusers`.`id` = `lead`.`added_by`)) ;
 
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indexes for table `data`
+-- Indexes for table `appusers`
 --
-ALTER TABLE `data`
+ALTER TABLE `appusers`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `users`
+-- Indexes for table `lead`
 --
-ALTER TABLE `users`
+ALTER TABLE `lead`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -87,16 +120,16 @@ ALTER TABLE `users`
 --
 
 --
--- AUTO_INCREMENT for table `data`
+-- AUTO_INCREMENT for table `appusers`
 --
-ALTER TABLE `data`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+ALTER TABLE `appusers`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
--- AUTO_INCREMENT for table `users`
+-- AUTO_INCREMENT for table `lead`
 --
-ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+ALTER TABLE `lead`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
