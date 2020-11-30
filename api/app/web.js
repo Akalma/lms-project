196b9d5de26852c34e2754858f46b1668a0b5488
Ubuntu <ubuntu@ip-172-31-40-164.ap-south-1.compute.internal>
2020-11-30 19:02:56 +0000

    Server side user id changed from email to anything

--- api/app/web.js
+++ api/app/web.js
@@ -29,8 +29,9 @@ module.exports = function (app) {
 
   app
     .post('/web/login', [
-      check('email').trim().isEmail().withMessage('Enter valid email').isLength({ min: 1 }).withMessage('Enter email address'),
-      check('password').trim().isLength({ min: 1 }).withMessage('Enter password')
+      // check('email').trim().isEmail().withMessage('Enter valid email').isLength({ min: 1 }).withMessage('Enter email address'),
+      check('email').trim().isLength({ min: 1 }).withMessage('Enter user id'),
+       check('password').trim().isLength({ min: 1 }).withMessage('Enter password')
     ], WebAppController.login)
 
     .post('/web/create-data', [
